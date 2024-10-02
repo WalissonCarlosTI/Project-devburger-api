397fdb89d5a9ca56ff2bfa820aafe70c06cde5f2
WalissonCarlosTI <walissoncarlosvip@gmail.com>
2024-10-02 20:09:37 -0300

    Projeto-devburger-api

---
+++ src/database/index.js
@@ -0,0 +1,38 @@
+import Sequelize from "sequelize";
+
+import mongoose from "mongoose";
+
+import configDatabase from "../config/database";
+
+import User from "../app/controllers/models/User";
+
+import Product from "../app/controllers/models/Product";
+
+import Category from "../app/controllers/models/Category";
+
+const models = [User, Product, Category];
+
+class Database {
+    constructor() {
+        this.init();
+        this.mongo();
+    };
+
+    init() {
+        this.connection = new Sequelize(configDatabase);
+        models
+            .map((model) => model.init(this.connection))
+            .map(
+                (model) => model.associate && model.associate(this.connection.models),
+            );
+    };
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            "mongodb://localhost:27017/devburger",
+        );
+    }
+
+};
+
+export default new Database();
---
+++ src/server.js
@@ -0,0 +1,6 @@
+import app from "./app"
+
+app.listen(3001, () => console.log("Server is running at port 3001..."))
+
+
+// rodar back = yarn dev
